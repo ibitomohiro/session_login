3795a1c80cedce3f1789868fc5efa8de6f7d6def
tomohiro ibi <68018348+ibitomohiro@users.noreply.github.com>
2020-09-10 22:13:19 +0900

    フィードバックメッセージの実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
     @blogs = Blog.all  
   end
@@ -18,27 +18,36 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.find(params[:id])
+    
   end
 
   def edit
-    @blog = Blog.find(params[:id])
+    
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました！"
     else
       render :edit
     end
   end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+
     
 
   private
   
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 
 end
